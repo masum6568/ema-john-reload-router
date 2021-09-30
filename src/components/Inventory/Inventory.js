0be574e97f30c036e558cedc91d4137f78fca135
masum6568 <xyzmasum1134@gmail.com>
2021-09-30 21:52:04 +0600

    first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h3>This is a inventory page
+
+            </h3>
+        </div>
+    );
+};
+
+export default Inventory;
